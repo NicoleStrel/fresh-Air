1a2d8f051d509c2791c813f7b0ab064582d8a912
Nicole Streltsov <streltsovnicole@gmail.com>
2020-08-06 13:27:37 -0400

    amplify

--- air-pollution-visual/src/components/Map.js
+++ air-pollution-visual/src/components/Map.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import "../style/style.css";
+import map from '../img/world.svg';
+
 class Map extends React.Component {
     render() {
         return (
             <div className="map">
-            <h1>MAP</h1>
-            <p>Map page body content</p>
-         </div>
+                <h1>MAP</h1>
+                <p>2017</p>
+
+                <img src={map} className="Map" alt="map" width="1300px"/>
+                <center>
+                <div class="gradientbar">
+                hi
+                </div>
+                </center>
+                
+                <p> HEIID</p>
+                <p> HEIID</p>
+                <p> HEIID</p>
+                <p> HEIID</p>
+            </div>
         )
      }
 }
