c98b53b7726ac3d72ed540abddc2e052ee8b5277
Nicole Streltsov <streltsovnicole@gmail.com>
2020-09-01 00:10:53 -0400

    added info on the main page

--- air-pollution-visual/src/components/Home.js
+++ air-pollution-visual/src/components/Home.js
@@ -3,7 +3,10 @@ import "../style/style.css";
 import logo from '../img/freshAir.png';
 import Map from './Map';
 import Stats from './Stats';
-
+import car from '../img/pickup-car.png';
+import fire from '../img/fire.png';
+import energy from '../img/energy.png';
+import bottle from '../img/bottle.png';
 
 class Home extends React.Component {
     
@@ -16,8 +19,8 @@ class Home extends React.Component {
                         <img src={logo}  className="main-logo" alt="logo" width="400px"  />
 
                         <div className="slidefirstCloud" id="cloud-circle"></div>
-
-                        <p className="underneath-text">Lorem ipsum dolor sit <br></br> amet, consectetur adipiscing elit.<br></br>  Nunc lacus sapien, mattis ac <br></br> </p>
+                          
+                        <p className="underneath-text">Humans have emitted<br></br>greenhouse gases starting<br></br>over 2,000 years ago.<br></br> </p>
 
                         <svg  width="0" height="0"> 
 
@@ -29,8 +32,7 @@ class Home extends React.Component {
                         </svg>
 
                         <div id="cloud-circle-2" className="slidesecondCloud"></div>
-
-                        <p className="underneath-text" id="cloud-2-under">Lorem ipsum dolor sit <br></br> amet, consectetur adipiscing elit.<br></br>  Nunc lacus sapien, mattis ac <br></br> </p>
+                        <p className="underneath-text" id="cloud-2-under">Air pollution is one of the larger<br></br>factors contributing to global warming.<br></br>What can we do to reduce it?<br></br> </p>
                         <svg width="0" height="0"> 
 
                             <filter id="filter-2">
@@ -44,65 +46,48 @@ class Home extends React.Component {
                     <br></br>
                     <br></br>
                     <div className="section-2 w-75">
-                        <h2>Our Misson- NEED TO FIXX</h2>
-                        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacus sapien, 
-                            mattis ac tellus et, venenatis consequat sem. Praesent laoreet justo sed nisi eleifend, 
-                            at laoreet nisl congue. Donec blandit urna sit amet faucibus porttitor. Praesent accumsan eros 
-                            finibus nisi tincidunt pellentesque. Mauris pellentesque orci non tellus semper, ut imperdiet libero 
-                            fermentum. Aenean porta fringilla varius. Integer nec lacinia justo.
-                            
-                            Maecenas ac vulputate velit. Phasellus ut auctor ante. Mauris neque libero, aliquam at imperdiet quis, 
-                            efficitur quis est. Mauris et nibh porta, rhoncus purus et, vehicula risus. Mauris blandit ipsum felis, 
-                            ut hendrerit ex finibus sed. Maecenas ac molestie tortor. Sed posuere ligula id lobortis imperdiet. Maecenas 
-                            ullamcorper nec nulla vitae placerat. Vestibulum placerat posuere dui sed molestie. Aliquam erat volutpat.
+                        <h2>Our Misson</h2>
+                        <span>To raise awareness about air pollution across the world and provide resources/visuals to analyze the 
+                            pollutants disposed of in the past. We believe that learning from the past and acknowledging our harmful 
+                            impact on the environment, we as a human race can move forward in creating a more sustainable and healthy 
+                            future for the next generation. 
+                            FreshAir provides the data, interpolation of trends, the ranking of countries, and an interactive map for discovering the effects of air pollution throughout the years.
                         </span>
                         <br></br>
                         <br></br>
                         <br></br>
-                        <h2>How Air pollution can be reduced</h2>
+                        <h2>How You Can Reduce Air Pollution</h2>
+                        <p>Change starts with you. Do not wait for governments or industries to start preventing pollution and global warming, its up to you to start. </p>
                         <br></br>
                         <div className="row">
                             <div className="col-md air-poll-list-item">
-                     
-                                <img src="https://picsum.photos/100??random=1" alt="sample-1"></img>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacus sapien, 
-                                    mattis ac tellus et, venenatis consequat sem. Praesent laoreet justo sed nisi eleifend, 
-                                    at laoreet nisl congue. Donec blandit urna sit amet faucibus porttitor.</p>
+                                <img src={car} alt="car-icon"></img>
+                                <span><strong>Maintain minimal pollution from your car.</strong> Reduce the number of trips you take with your car, carpool/bike/walk whenever you can, consider zero or near-zero emission cars, and keep your car in good shape- to prevent unnecessary smog coming from your exhaust pipe.
+                                </span>
                             </div>
                             <br></br>
                             <div className="col-md air-poll-list-item">
-                                <img src="https://picsum.photos/100?random=2" alt="sample-2"></img>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacus sapien, 
-                                    mattis ac tellus et, venenatis consequat sem. Praesent laoreet justo sed nisi eleifend, 
-                                    at laoreet nisl congue. Donec blandit urna sit amet faucibus porttitor.</p>
+                                <img src={fire} alt="fire-icon"></img>
+                                <span><strong>Reduce fireplace or woodstove use.</strong> Smog is created from open flames so the use of these things should be minimal. Consider using electric lighters and lighter fluid that produces fewer emissions than traditional charcoal lighter fluid to start the fires.</span>
                             </div>
                             <br></br>
                         </div>
-                        
+                        <br></br>
+                        <br></br>
                         <div className="row">
                             <div className="col-md air-poll-list-item">
-
-                                <img src="https://picsum.photos/100?random=3" alt="sample-3"></img>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacus sapien, 
-                                    mattis ac tellus et, venenatis consequat sem. Praesent laoreet justo sed nisi eleifend, 
-                                    at laoreet nisl congue. Donec blandit urna sit amet faucibus porttitor.</p>
+                                <img src={energy} alt="energy-icon"></img>
+                                <span><strong>Reconsider how you use your energy.</strong> Install for energy-efficient lighting and appliances, use the thermostat wisely (eg. make your thermostat a few degrees cooler in the winter, and consider using solar power or other eco-friendly energy sources.
+                                </span>
                             </div>
                             <br></br>
                             <div className="col-md air-poll-list-item">
-
-                                <img src="https://picsum.photos/100?random=4" alt="sample-4"></img>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacus sapien, 
-                                    mattis ac tellus et, venenatis consequat sem. Praesent laoreet justo sed nisi eleifend, 
-                                    at laoreet nisl congue. Donec blandit urna sit amet faucibus porttitor.</p>
+                                <img src={bottle} alt="bottle-icon"></img>
+                                <span><strong>Take care in what products you use.</strong> Avoid or minimize aerosol products-that contribute to smog, and instead look for water-based options. As well, buy products that are reusable and avoid plastic water bottles due to their waste and harmful factory production. </span>
                             </div>
        
                         </div>
-                            <br></br>
                         <br></br>
-                        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacus sapien, 
-                            mattis ac tellus et, venenatis consequat sem. Praesent laoreet justo sed nisi eleifend, 
-                            at laoreet nisl congue. Donec blandit urna sit amet faucibus porttitor.</span>
-                            <br></br>
                         <br></br>
                         <br></br>
 
